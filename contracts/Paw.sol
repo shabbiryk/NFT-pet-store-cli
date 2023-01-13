2995137bbd4aec7e71139a1f475b9f00db3fe08a
shabbiryk.eth <shabbirkhand.sk@gmail.com>
2023-01-13 23:09:22 +0530

    Optimized: Paw.sol

--- contracts/Paw.sol
+++ contracts/Paw.sol
@@ -1,11 +1,9 @@
-/// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Paw is ERC20, Ownable {
-
   uint256 private airDropAmount = 500;
 
   constructor() ERC20("Paw", "PAW") {
@@ -13,12 +11,14 @@ contract Paw is ERC20, Ownable {
   }
 
   function requestAirdrop() public {
+    require(balanceOf(msg.sender) > airDropAmount, "Insufficient balance.");
     airdropTo(msg.sender, airDropAmount);
   }
 
-  function airdropTo(address recipient, uint256 amount)
-    private
-  {
-    _transfer(owner(), recipient, amount);
+  function airdropTo(address payable recipient, uint256 amount) private {
+    require(transfer(owner(), recipient, amount), "Transfer failed.");
+    emit Airdrop(msg.sender, recipient, amount);
   }
+
+  event Airdrop(address indexed sender, address indexed recipient, uint256 amount);
 }
